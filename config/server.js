cfc88881613101861ab254096aaccb04cfb968e2
gbenimako@gmail.com <gbenimako@gmail.com>
2020-09-17 18:25:21 +0200

    initials

---
+++ config/server.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ACCESS_KEY:
+    process.env.ACCESS_KEY || "prdbusXAIk8IszKEIZ6moIAvnvPgGkPuUAjoK02hpZo",
+  SECRET: process.env.SECRET || "FSL6K85ykGiqHLTELgc80EyJZVIYYig8QdiZCxt5nLw",
+  CALLBACK_URL: process.env.CALLBACK_URL || "http://localhost:3000",
+};
