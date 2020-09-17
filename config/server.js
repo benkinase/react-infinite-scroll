81b33b9d5697bcab6742b414fda4e7f75f6b22fc
gbenimako@gmail.com <gbenimako@gmail.com>
2020-09-17 18:43:29 +0200

    server

--- config/server.js
+++ config/server.js
@@ -1,6 +1,5 @@
 module.exports = {
-  ACCESS_KEY:
-    process.env.ACCESS_KEY || "prdbusXAIk8IszKEIZ6moIAvnvPgGkPuUAjoK02hpZo",
-  SECRET: process.env.SECRET || "FSL6K85ykGiqHLTELgc80EyJZVIYYig8QdiZCxt5nLw",
+  ACCESS_KEY: process.env.ACCESS_KEY || "",
+  SECRET: process.env.SECRET || "",
   CALLBACK_URL: process.env.CALLBACK_URL || "http://localhost:3000",
 };
